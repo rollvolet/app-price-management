a6eac9a23750256ce1e410440848272c19a3fa45
Erika Pauwels <erika.pauwels@gmail.com>
2020-10-04 17:30:15 +0200

    Add delta-notifier and setup mu-cl-resources cache clearing

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -6,5 +6,8 @@
 (defparameter *include-count-in-paginated-responses* t
   "when non-nil, all paginated listings will contain the number
    of responses in the result object's meta.")
+(defparameter *cache-count-queries* t)
+(defparameter *cache-model-properties* t)
+(setf sparql:*experimental-no-application-graph-for-sudo-select-queries* t)
 
 (read-domain-file "products.json")
