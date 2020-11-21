632b06019e581c2d6934ed59758b29407cb809ec
Erika Pauwels <erika.pauwels@gmail.com>
2020-11-21 15:23:34 +0100

    Make mu-search listen to delta notifications

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,11 +1,25 @@
 export default [
   {
     match: {
-      subject: { }
+      // listen to all changes
     },
     callback: {
       url: "http://resource/.mu/delta",
       method: "POST"
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 250,
+      ignoreFromSelf: true
+    }
+  },
+  {
+    match: {
+      // listen to all changes
+    },
+    callback: {
+      url: 'http://search/update',
+      method: 'POST'
     },
     options: {
       resourceFormat: "v0.0.1",
