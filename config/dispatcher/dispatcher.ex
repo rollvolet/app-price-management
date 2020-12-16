acbbf511f9d8972b99d6d99f7c854d0c82a56b70
Erika Pauwels <erika.pauwels@gmail.com>
2020-12-16 10:20:26 +0100

    Add frontend service and host behind dispatcher

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -7,104 +7,109 @@ defmodule Dispatcher do
     any: [ "*/*" ]
   ]
 
-  define_layers [ :static, :api, :frontend_fallback, :not_found ]
+  define_layers [ :static, :frontend, :api, :frontend_fallback, :not_found ]
 
-  @json %{ accept: %{ json: true }, layer: :api }
+  @json_service %{ layer: :api, accept: %{ json: true } }
+  @html_pages %{ layer: :frontend, accept: %{ html: true } }
 
-  match "/sessions/*path", @json do
-    Proxy.forward conn, path, "http://login/sessions/"
-  end
-
-  get "/products/search/*path", @json do
-    Proxy.forward conn, path, "http://search/products/search/"
-  end
-
-  match "/business-entities/*path", @json do
-    Proxy.forward conn, path, "http://cache/business-entities/"
-  end
-
-  match "/addresses/*path", @json do
-    Proxy.forward conn, path, "http://cache/addresses/"
-  end
-
-  match "/contact-points/*path", @json do
-    Proxy.forward conn, path, "http://cache/contact-points/"
-  end
-
-  match "/offerings/*path", @json do
-    Proxy.forward conn, path, "http://cache/offerings/"
-  end
-
-  match "/products/*path", @json do
-    Proxy.forward conn, path, "http://cache/products/"
-  end
-
-  match "/unit-price-specifications/*path", @json do
-    Proxy.forward conn, path, "http://cache/unit-price-specifications/"
-  end
-
-  match "/warehouse-locations/*path", @json do
-    Proxy.forward conn, path, "http://cache/warehouse-locations/"
-  end
-
-  match "/warehouse-departments/*path", @json do
-    Proxy.forward conn, path, "http://cache/warehouse-departments/"
-  end
-
-  match "/business-entity-types/*path", @json do
-    Proxy.forward conn, path, "http://cache/business-entity-types/"
-  end
-
-  match "/unit-codes/*path", @json do
-    Proxy.forward conn, path, "http://cache/unit-codes/"
-  end
-
-  match "/product-categories/*path", @json do
-    Proxy.forward conn, path, "http://cache/product-categories/"
-  end
-
-  match "/organization-types/*path", @json do
-    Proxy.forward conn, path, "http://cache/organization-types/"
-  end
-
-  match "/languages/*path", @json do
-    Proxy.forward conn, path, "http://cache/languages/"
-  end
-
-  match "/countries/*path", @json do
-    Proxy.forward conn, path, "http://cache/countries/"
-  end
-
-  post "/files/*path", @json do
-    Proxy.forward conn, path, "http://file/files/"
-  end
-
-  delete "/files/*path", @json do
-    Proxy.forward conn, path, "http://file/files/"
-  end
-
-  get "/files/:id/dowload", @json do
-    Proxy.forward conn, [], "http://file/files/" <> id <> "/download"
-  end
-
-  get "/files/*path", @json do
-    Proxy.forward conn, path, "http://resource/files/"
-  end
-
-  patch "/files/*path", @json do
-    Proxy.forward conn, path, "http://resource/files/"
-  end
-
-  post "/sequence-numbers/*path", @json do
-    Proxy.forward conn, path, "http://sequence-numbers/sequence-numbers/"
-  end
+  # get "/*_path", %{ layer: :frontend, accept: %{ html: true } } do
+  #   Proxy.forward conn, [], "http://frontend/index.html"
+  # end
 
   get "/assets/*path", %{ layer: :static } do
     Proxy.forward conn, path, "http://frontend/assets/"
   end
 
-  get "/*_path", %{ layer: :frontend_fallback } do
+  get "/*_path", @html_pages do
     Proxy.forward conn, [], "http://frontend/index.html"
+  end
+
+  match "/sessions/*path", @json_service do
+    Proxy.forward conn, path, "http://login/sessions/"
+  end
+
+  get "/products/search/*path", @json_service do
+    Proxy.forward conn, path, "http://search/products/search/"
+  end
+
+  match "/business-entities/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/business-entities/"
+  end
+
+  match "/addresses/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/addresses/"
+  end
+
+  match "/contact-points/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/contact-points/"
+  end
+
+  match "/offerings/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/offerings/"
+  end
+
+  match "/products/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/products/"
+  end
+
+  match "/unit-price-specifications/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/unit-price-specifications/"
+  end
+
+  match "/warehouse-locations/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/warehouse-locations/"
+  end
+
+  match "/warehouse-departments/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/warehouse-departments/"
+  end
+
+  match "/business-entity-types/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/business-entity-types/"
+  end
+
+  match "/unit-codes/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/unit-codes/"
+  end
+
+  match "/product-categories/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/product-categories/"
+  end
+
+  match "/organization-types/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/organization-types/"
+  end
+
+  match "/languages/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/languages/"
+  end
+
+  match "/countries/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/countries/"
+  end
+
+  post "/files/*path", @json_service do
+    Proxy.forward conn, path, "http://file/files/"
+  end
+
+  delete "/files/*path", @json_service do
+    Proxy.forward conn, path, "http://file/files/"
+  end
+
+  get "/files/:id/dowload", @json_service do
+    Proxy.forward conn, [], "http://file/files/" <> id <> "/download"
+  end
+
+  get "/files/*path", @json_service do
+    Proxy.forward conn, path, "http://resource/files/"
+  end
+
+  patch "/files/*path", @json_service do
+    Proxy.forward conn, path, "http://resource/files/"
+  end
+
+  post "/sequence-numbers/*path", @json_service do
+    Proxy.forward conn, path, "http://sequence-numbers/sequence-numbers/"
   end
 
   match "/*_path", %{ last_call: true, accept: %{ json: true } } do
