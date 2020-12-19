ce5275b67bcc915c6111fb999b05939595ff8b0d
Erika Pauwels <erika.pauwels@gmail.com>
2020-12-19 19:23:55 +0100

    Add reset-elastic script

---
+++ scripts/reset-elastic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "[WARNING] This will run queries on the triplestore and delete containers, you have 3 seconds to press ctrl+c"
+sleep 3
+docker-compose rm -fs elasticsearch search
+docker-compose rm -fs kibana
+sudo rm -rf data/elasticsearch/
+docker-compose exec -T virtuoso isql-v <<EOF
+SPARQL DELETE WHERE {   GRAPH <http://mu.semte.ch/authorization> {     ?s ?p ?o.   } };
+exec('checkpoint');
+exit;
+EOF
+docker-compose up -d --remove-orphans
