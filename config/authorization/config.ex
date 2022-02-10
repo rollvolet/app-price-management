f13eca2b302862fe91c11bf4846f4cb608bd6ba2
Erika Pauwels <erika.pauwels@gmail.com>
2022-02-10 19:59:13 +0100

    Update authorization config to ensure user is logged in

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -1,10 +1,23 @@
 alias Acl.Accessibility.Always, as: AlwaysAccessible
+alias Acl.Accessibility.ByQuery, as: AccessByQuery
 alias Acl.GraphSpec.Constraint.Resource, as: ResourceConstraint
 alias Acl.GraphSpec, as: GraphSpec
 alias Acl.GroupSpec, as: GroupSpec
 alias Acl.GroupSpec.GraphCleanup, as: GraphCleanup
 
 defmodule Acl.UserGroups.Config do
+
+  defp authenticated_access() do
+    %AccessByQuery{
+      vars: [],
+      query: "PREFIX session: <http://mu.semte.ch/vocabularies/session/>
+      PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+      SELECT ?account WHERE {
+          <SESSION_ID> session:account ?account .
+      } LIMIT 1"
+    }
+  end
+
   def user_groups do
     # These elements are walked from top to bottom.  Each of them may
     # alter the quads to which the current query applies.  Quads are
@@ -16,28 +29,32 @@ defmodule Acl.UserGroups.Config do
       %GroupSpec{
         name: "rollvolet",
         useage: [:read, :write, :read_for_write],
-        access: %AlwaysAccessible{}, # TODO check related account
-        graphs: [ %GraphSpec{
-                    graph: "http://mu.semte.ch/graphs/rollvolet",
-                    constraint: %ResourceConstraint{
-                      resource_types: [
-                        "http://purl.org/goodrelations/v1#BusinessEntity",
-                        "http://schema.org/ContactPoint",
-                        "http://www.w3.org/ns/locn#Address",
-                        "http://purl.org/goodrelations/v1#Offering",
-                        "http://purl.org/goodrelations/v1#SomeItems",
-                        "http://purl.org/goodrelations/v1#UnitPriceSpecification",
-                        "http://data.rollvolet.be/vocabularies/stock-management/WarehouseLocation",
-                        "http://data.rollvolet.be/vocabularies/stock-management/WarehouseDepartment",
-                        "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject",
-                        "http://purl.org/goodrelations/v1#BusinessEntityType",
-                        "http://mu.semte.ch/vocabularies/ext/UnitCode",
-                        "http://mu.semte.ch/vocabularies/ext/ProductCategory",
-                        "http://mu.semte.ch/vocabularies/ext/OrganizationType",
-                        "http://schema.org/Country",
-                        "http://schema.org/Language"
-                      ]
-                    } } ] },
+        access: authenticated_access(),
+        graphs: [
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/rollvolet",
+            constraint: %ResourceConstraint{
+              resource_types: [
+                "http://purl.org/goodrelations/v1#BusinessEntity",
+                "http://schema.org/ContactPoint",
+                "http://www.w3.org/ns/locn#Address",
+                "http://purl.org/goodrelations/v1#Offering",
+                "http://purl.org/goodrelations/v1#SomeItems",
+                "http://purl.org/goodrelations/v1#UnitPriceSpecification",
+                "http://data.rollvolet.be/vocabularies/stock-management/WarehouseLocation",
+                "http://data.rollvolet.be/vocabularies/stock-management/WarehouseDepartment",
+                "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject",
+                "http://purl.org/goodrelations/v1#BusinessEntityType",
+                "http://mu.semte.ch/vocabularies/ext/UnitCode",
+                "http://mu.semte.ch/vocabularies/ext/ProductCategory",
+                "http://mu.semte.ch/vocabularies/ext/OrganizationType",
+                "http://schema.org/Country",
+                "http://schema.org/Language"
+              ]
+            }
+          }
+        ]
+      },
 
       # // CLEANUP
       #
