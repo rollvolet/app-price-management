cc026c1899aeac071721e065bf410be924904a57
Erika Pauwels <erika.pauwels@gmail.com>
2024-01-19 15:17:31 +0100

    Structure dispatcher config

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -12,6 +12,13 @@ defmodule Dispatcher do
   @json_service %{ layer: :api, accept: %{ json: true } }
   @html_pages %{ layer: :frontend, accept: %{ html: true } }
 
+
+  ## Static frontend resources of all kinds
+
+  get "/favicon.ico", %{ layer: :static } do
+    Proxy.forward conn, [], "http://frontend/favicon.ico"
+  end
+
   get "/assets/*path", %{ layer: :static } do
     Proxy.forward conn, path, "http://frontend/assets/"
   end
@@ -20,21 +27,51 @@ defmodule Dispatcher do
     Proxy.forward conn, [], "http://frontend/torii/redirect.html"
   end
 
-  get "/files/:id/download", %{ layer: :static } do
-    Proxy.forward conn, [], "http://file/files/" <> id <> "/download"
-  end
 
-  get "/*_path", @html_pages do
-    Proxy.forward conn, [], "http://frontend/index.html"
-  end
+  ## Authentication / login
 
   match "/sessions/*path", @json_service do
     Proxy.forward conn, path, "http://msal-login/sessions/"
   end
 
+  ## File upload/download
+
+  get "/files/:id/download", %{ layer: :static } do
+    Proxy.forward conn, [], "http://file/files/" <> id <> "/download"
+  end
+
+  post "/files/*path", @json_service do
+    Proxy.forward conn, path, "http://file/files/"
+  end
+
+  delete "/files/*path", @json_service do
+    Proxy.forward conn, path, "http://file/files/"
+  end
+
+  get "/files/*path", @json_service do
+    Proxy.forward conn, path, "http://resource/files/"
+  end
+
+  patch "/files/*path", @json_service do
+    Proxy.forward conn, path, "http://resource/files/"
+  end
+
+
+  ## Sequence numbers
+
+  post "/sequence-numbers/*path", @json_service do
+    Proxy.forward conn, path, "http://sequence-numbers/sequence-numbers/"
+  end
+
+
+  ## Search
+
   get "/products/search/*path", @json_service do
     Proxy.forward conn, path, "http://search/products/search/"
   end
+
+
+  ## Regular resources
 
   match "/business-entities/*path", @json_service do
     Proxy.forward conn, path, "http://cache/business-entities/"
@@ -64,52 +101,39 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://cache/warehouse-locations/"
   end
 
-  match "/warehouse-departments/*path", @json_service do
-    Proxy.forward conn, path, "http://cache/warehouse-departments/"
-  end
-
   match "/business-entity-types/*path", @json_service do
     Proxy.forward conn, path, "http://cache/business-entity-types/"
-  end
-
-  match "/unit-codes/*path", @json_service do
-    Proxy.forward conn, path, "http://cache/unit-codes/"
   end
 
   match "/product-categories/*path", @json_service do
     Proxy.forward conn, path, "http://cache/product-categories/"
   end
 
-  match "/organization-types/*path", @json_service do
+  get "/warehouse-departments/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/warehouse-departments/"
+  end
+
+  get "/unit-codes/*path", @json_service do
+    Proxy.forward conn, path, "http://cache/unit-codes/"
+  end
+
+  get "/organization-types/*path", @json_service do
     Proxy.forward conn, path, "http://cache/organization-types/"
   end
 
-  match "/languages/*path", @json_service do
+  get "/languages/*path", @json_service do
     Proxy.forward conn, path, "http://cache/languages/"
   end
 
-  match "/countries/*path", @json_service do
+  get "/countries/*path", @json_service do
     Proxy.forward conn, path, "http://cache/countries/"
   end
 
-  post "/files/*path", @json_service do
-    Proxy.forward conn, path, "http://file/files/"
-  end
 
-  delete "/files/*path", @json_service do
-    Proxy.forward conn, path, "http://file/files/"
-  end
+  ## Fallback
 
-  get "/files/*path", @json_service do
-    Proxy.forward conn, path, "http://resource/files/"
-  end
-
-  patch "/files/*path", @json_service do
-    Proxy.forward conn, path, "http://resource/files/"
-  end
-
-  post "/sequence-numbers/*path", @json_service do
-    Proxy.forward conn, path, "http://sequence-numbers/sequence-numbers/"
+  get "/*_path", @html_pages do
+    Proxy.forward conn, [], "http://frontend/index.html"
   end
 
   match "/*_path", %{ last_call: true, accept: %{ json: true } } do
